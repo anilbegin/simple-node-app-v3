dba8c3fda002063414b64a85eda905b04b3dd6a4
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-06-10 16:19:19 +0530

    additional validations for user registration

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 let usersCollection = require('../db').db().collection('users')
+const validator = require("validator")
 
 let User = function(data) {
   this.data = data
@@ -21,6 +22,14 @@ User.prototype.validate = function() {
   if(this.data.username == "") this.errors.push("Username cannot be blank")
   if(this.data.email == "") this.errors.push("Email cannot be blank")
   if(this.data.password == "") this.errors.push("Password cannot be blank")
+
+  if(this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push('Username must be atleast 3 characters')}
+  if(this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters")}
+  if(this.data.password.length > 0 && this.data.password.length < 6) {this.errors.push('Password must be atleast 6 characters')}
+  if(this.data.password.length > 30) {this.errors.push("Password cannot exceed 30 characters")}
+
+  if(this.data.username != "" && !validator.isAlphanumeric(this.data.username))  {this.errors.push('Username can only contain letters and numbers')}
+  if(!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
 }
 
 User.prototype.register = async function() {
@@ -29,7 +38,7 @@ User.prototype.register = async function() {
   
   if(!this.errors.length) {
     await usersCollection.insertOne(this.data)
-    console.log('new user created')
+  //  console.log('new user created')
   }
 }
 
