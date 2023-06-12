54e476a01a73da26e8e40fc8576d3b2609d2ef00
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-06-12 15:51:21 +0530

    displaying the new timestampfor the Updated Note, on the fly

--- public/browser.js
+++ public/browser.js
@@ -3,7 +3,15 @@ document.addEventListener('click', e => {
    let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
    if(userInput) {
       axios.post('/edit-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function() {
+        
+        // displaying the new timestamp for Updated Note, on the fly
+        const date = new Date()
+        let theMinute = date.getMinutes()
+        theMinute = theMinute < 10 ? '0' + theMinute : theMinute 
+        let theDate = '(' + date.getDate() + '/' + `${date.getMonth() + 1}` + '/' + date.getFullYear() + ', ' + date.getHours() + ':' + theMinute + ')'
+
         e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
+        e.target.parentElement.parentElement.querySelector(".item-date").innerHTML = theDate
     }).catch(function() {
         console.log("Please try again later")
     })
