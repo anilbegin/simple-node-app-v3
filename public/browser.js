c4584b28f3e1be46ff980c7782f46798eaab517f
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-06-12 11:37:18 +0530

    testing whether click events edit/delete buttons are responding

--- public/browser.js
+++ public/browser.js
@@ -1,1 +1,11 @@
-alert('hello from browser.js')
+document.addEventListener('click', e => {
+  if(e.target.classList.contains("edit-me")) {
+    alert('edit button got clicked')
+  }
+})
+
+document.addEventListener('click', e => {
+  if(e.target.classList.contains("delete-me")) {
+    alert('delete button got clicked')
+  }
+})
